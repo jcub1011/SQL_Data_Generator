6dc4b31f81bac3fd3ffa3acd0a1cec06379f5866
Jacob McCormack <91000914+jcub1011@users.noreply.github.com>
2024-02-26 13:56:39 -0500

    Working on random name generator.

---
+++ RandomNameGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Numerics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SQL_Data_Generator;
+
+public static class RandomNameGenerator
+{
+    static List<string> _names = new();
+    static Random _rand = new();
+
+    /// <summary>
+    /// Returns (firstName, lastName)
+    /// </summary>
+    /// <returns></returns>
+    static public (string first, string last) GetName()
+    {
+        return ("Dave", "Duncan");
+    }
+}
