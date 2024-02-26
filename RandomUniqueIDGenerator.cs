9a272e431ee9c43f2f4d17d341b3e75f51cf6076
Jacob McCormack <91000914+jcub1011@users.noreply.github.com>
2024-02-26 12:38:11 -0500

    Added unique id generator.

---
+++ RandomUniqueIDGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Data_Generator;
+
+public class RandomUniqueIDGenerator
+{
+    HashSet<int> _existingNums;
+    Random _rand;
+
+    public RandomUniqueIDGenerator()
+    {
+        _existingNums = new();
+        _rand = new((int)DateTime.Now.Ticks);
+    }
+
+    public int GetUniqueID(int maxValue = int.MaxValue)
+    {
+        int value = _rand.Next(maxValue);
+
+        while (_existingNums.Contains(value))
+        {
+            _rand.Next(maxValue);
+        }
+
+        _existingNums.Add(value);
+        return value;
+    }
+}
